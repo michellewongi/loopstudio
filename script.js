3f18e519f3d2c68ab2691e2c54cacca9735c1dfe
mw3981 <mw3981@nyu.edu>
2021-07-21 03:02:06 -0400

    First push to Github
    Finished the mobile layout and functional hamburger menu

---
+++ script.js
@@ -0,0 +1,27 @@
+var menu = document.querySelector(".wrapper");
+var ham = document.querySelector(".hamburger");
+var xIcon = document.querySelector(".close-icon");
+var hamIcon = document.querySelector(".hamburger-icon");
+
+ham.addEventListener("click", toggleMenu);
+menu.classList.contains("show-menu");
+
+function toggleMenu() {
+  if (menu.classList.contains("show-menu")) {
+    menu.classList.remove("show-menu");
+    xIcon.style.display = "none";
+    hamIcon.style.display = "block";
+  } else {
+    menu.classList.add("show-menu");
+    xIcon.style.display = "block";
+    hamIcon.style.display = "none";
+  }
+}
+
+var menuLinks = document.querySelectorAll(".menu-links");
+
+menuLinks.forEach(
+  function(menuLink) {
+    menuLink.addEventListener("click", toggleMenu);
+  }
+)
